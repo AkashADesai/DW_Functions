2ec172659c9d354c21781a33ba2e9f62c2d29fca
Akash Desai <Akash.a.desai@gmail.com>
2017-01-23 19:52:12 -0800

    Adding R script MasterControl
    R file with hard-coded links to DW Python Scripts

---
+++ MasterControl.R
@@ -0,0 +1,18 @@
+
+### Python Utils ###
+## Use the following General Syntax for Running Python Scripts in R
+
+#cmd.1 <- paste("python",PythonScriptName,param1,param2,etc,sep=" ")
+#system(cmd.1)
+
+#CopyPasteOverWrite
+CopyPasteOverwrite <-"/Users/akashdesai/Akash_DW/mastercode/Github/DW_Functions/CopyPasteOverwrite.py"
+
+#cmd.1 <- paste("python",CopyPasteOverwrite,Scrpath,destpath,pathstoMove,sep=" ")
+#system(cmd.1)
+
+#Create Glacier Vault
+Create_GlacierVault <-"/Users/akashdesai/Akash_DW/mastercode/Github/DW_Functions/Create_GlacierVault.py"
+
+#cmd.1 <- paste("python",Create_GlacierVault,VaultName,sep=" ")
+#system(cmd.1)
